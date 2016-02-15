afcee52db1614f590d0e7c4f0c49a9710f6c8102
Joseph Kahn <josephkahn@college.harvard.edu>
2016-02-15 14:06:46 -0500

    Joe learns things.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void hello_aron()
+{
+  cout << "Aron is amazeballs!\n";
+}
+
+int main()
+{
+  cout << "Hello World!\n";
+  hello_aron();
+  return 0;
+}
