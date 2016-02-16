58131ac4133ef768224d1b0cd604620283ae7720
Joseph Kahn <josephkahn@college.harvard.edu>
2016-02-16 00:37:42 -0500

    Removing now irrelevant comments

--- MinSpanTree/main.cpp
+++ MinSpanTree/main.cpp
@@ -12,17 +12,16 @@ const auto clock_seed = static_cast<size_t>(clock());
 const size_t pid_seed = hash<thread::id>()(this_thread::get_id());
 seed_seq seed_val { time_seed, clock_seed, pid_seed };
 mt19937_64 rand_gen;
-// TODO Could have a separate 'subset' struct which just keeps track of rank and parent for every edge in the edges vector
 
 typedef struct Vertex {
-    
+
     vector<double> coords;
     Vertex* parent;
     int rank;
 } Vertex;
 
 typedef struct Edge {
-    
+
     Vertex* u;
     Vertex* v;
     double distance;
@@ -40,26 +39,26 @@ typedef struct Graph {
 } Graph;
 
 double generateRandomVal() {
-    
+
     return generate_canonical<double, 50>(rand_gen);
 }
 
 Vertex* generateRandomVertex(int dimensions) {
-    
-    // initialize vertex, make self the parent, and set rank to one
+
+    // Initialize vertex, make self the parent, and set rank to one
     Vertex* vertex = new Vertex();
     vertex->parent = vertex;
     vertex->rank = 1;
-    
+
     vector<double> coords(dimensions);
-    
-    // initialize coordinates to random vector in given number of dimensions
+
+    // Initialize coordinates to random vector in given number of dimensions
     for (int i = 0; i < dimensions; i++) {
         coords[i] = (generateRandomVal());
     }
-    
+
     vertex->coords = coords;
-    
+
     return vertex;
 }
 
@@ -71,22 +70,20 @@ double calcEuclideanDist(Vertex* u, Vertex* v) {
 }
 
 Graph generateGraph(long size, int dimensions) {
-    
+
     vector<Vertex*> vertices(size);
     vector<Edge*> edges(size * (size - 1) / 2);
-    
+
     // Generate vertices
     for (int i = 0; i < size; i++) {
         vertices[i] = generateRandomVertex(dimensions);
     }
-    
-    // Instantiate all (n choose 2) edges.
-    // TODO edges.reserve();
+
     long edge_count = 0;
     for (int i = 0; i < size; i++) {
-        
+
         for (int j = i + 1; j < size; j++) {
-            
+
             Vertex* u = vertices[i];
             Vertex* v = vertices[j];
             double distance = calcEuclideanDist(u,v);
@@ -94,9 +91,7 @@ Graph generateGraph(long size, int dimensions) {
             edges[edge_count++] = new_edge;
         }
     }
-    
-    // TODO Sort edges. Maybe with custom sort algorithm.
-    
+
     return (Graph) {(int) vertices.size(), (int) edges.size(), edges, vertices};
 }
 
@@ -112,7 +107,7 @@ void set_union(Vertex* v, Vertex* u){
     if (v == u){
         return;
     }
-    
+
     if (v_root->rank < u_root->rank){
         v_root->parent = u_root;
     }
@@ -123,7 +118,7 @@ void set_union(Vertex* v, Vertex* u){
         u_root->parent = v_root;
         v_root->rank++;
     }
-    
+
 }
 
 vector<Edge*> findMST(Graph &G){
@@ -140,16 +135,8 @@ vector<Edge*> findMST(Graph &G){
 
 int main(){
     rand_gen.seed(seed_val);
-    
-    // NB for Jozi- "auto" type is automatic typing, ie best guess (closest to dynamic typing as it gets in cpp)
+
     auto G = generateGraph(4, 4);
     auto MST = findMST(G);
     return 0;
 }
-
-// TODO It makes sense to keep track of parents using indices or pointers but not both.
-// findSet(vector<Edge*> edges, index) {
-//   if (edges[i].parent != &edges[i]) {
-//     edges[i].parent = findSet(edges, parents_index)
-//   }
-// }
